6f1ad1a15cab7def78aa9068c8ea6285e9a6b055
Vivian Chow <zbs4ms@163.com>
2017-08-14 14:58:18 +0800

    Update DoctorMapper.java
    fix bug

--- reservation/src/main/java/com/jishi/reservation/dao/mapper/DoctorMapper.java
+++ reservation/src/main/java/com/jishi/reservation/dao/mapper/DoctorMapper.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Service;
 
 public interface DoctorMapper extends MyMapper<Doctor> {
 
-    @Service({" select * from doctor " +
-            " where 1=1 " +
-            " <if test='vendorKw != null'> " +
-            "     AND " +
-            " </if> " +
-            " name like '%#{name}%' "})
+   // @Service({" select * from doctor " +
+     //       " where 1=1 " +
+   //         " <if test='vendorKw != null'> " +
+   //         "     AND " +
+   //         " </if> " +
+   //         " name like '%#{name}%' "})
 
 }
