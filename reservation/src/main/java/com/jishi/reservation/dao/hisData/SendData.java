9954d6824bc0abc9ef1933a372eac22af548d6ec
c568254965 <568254965@qq.com>
2017-10-13 03:41:00 +0800

    注册对接his

--- reservation/src/main/java/com/jishi/reservation/dao/hisData/SendData.java
+++ reservation/src/main/java/com/jishi/reservation/dao/hisData/SendData.java
@@ -25,7 +25,7 @@ public class SendData {
 
     static final String url = "http://192.168.100.13:8086/EXTERNALSERVICES/ZL_INFORMATIONSERVICE.ASMX/";
 
-    public static void main(String[] args) throws Exception {
+//    public static void main(String[] args) throws Exception {
 //        String xmlInput =toXml("","Basic.MCTest.Query");
 //        JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
 //        String wsUrl = "http://192.168.100.13:8086/ExternalServices/ZL_InformationService.asmx?wsdl";
@@ -40,27 +40,27 @@ public class SendData {
 //        }
 //        System.exit(0);
 //        // END SNIPPET: client
-
-        String a = "  <DJH>R0664518,R0664519,R0664756,R0664757,R0664758,R0664759,R0664760,R0664761</DJH>\n" +
-                "  <JE>488.50</JE>\n" +
-                "  <SFGH>0</SFGH>\n" +
-                "  <BRID>810553</BRID>\n" +
-                "  <JSLIST>\n" +
-                "   <JS>\n" +
-                "    <JSKLB>21</JSKLB>\n" +
-                "    <JSKH/>\n" +
-                "    <JSFS>weixin_wap</JSFS>\n" +
-                "    <JSJE>488.50</JSJE>\n" +
-                "    <JYLSH>4003632001201709141980077107</JYLSH>\n" +
-                "   </JS>\n" +
-                "  </JSLIST>";
-
-        String b = "Bla7xAv4MZzg9KU+T8ASuFCNnxa2rBtfCuCaRaU1CGqjjV4K/KtOCFUp0z2wgJrKzikhBtNaMvWEYOzWqBuYf8xLESRbm77Q/p6bbx7Scv0vUjX/a3TGbOB63DmCfPqUjE1N+wf74qIoaEF2o5bzQxRX2mCLtHSXI1n3dLeVXD4XxlLFBpfp8vp7hzBWhbiLSp/yQ2j23TF1SQ2yxwoluHp961kLQgMUqwA+1SSWAC8DYxlN599NTe/pEWdz0YYY9tbpIEFGNyArtNIXCmtjiT3i9cZhPqDndj72awGcGUizXfmEg7JGbzq1qgwlMHAyWP2LwXoOSStW1oxs+iOBbW0ZmzBgQC6CiExAv4H6RjPycedFAxNRgEuKSXScuTIwAOpNF3pjoRDmxzgR3WLMR+i3aElZk9CmOMHQn9y0lRA=";
-        String c = Codec.Encrypt(a);
-        System.out.println(Codec.Encrypt(a));
-        System.out.println(Codec.Decrypt(c));
-        System.out.println(Codec.Encrypt(token));
-    }
+//
+//        String a = "  <DJH>R0664518,R0664519,R0664756,R0664757,R0664758,R0664759,R0664760,R0664761</DJH>\n" +
+//                "  <JE>488.50</JE>\n" +
+//                "  <SFGH>0</SFGH>\n" +
+//                "  <BRID>810553</BRID>\n" +
+//                "  <JSLIST>\n" +
+//                "   <JS>\n" +
+//                "    <JSKLB>21</JSKLB>\n" +
+//                "    <JSKH/>\n" +
+//                "    <JSFS>weixin_wap</JSFS>\n" +
+//                "    <JSJE>488.50</JSJE>\n" +
+//                "    <JYLSH>4003632001201709141980077107</JYLSH>\n" +
+//                "   </JS>\n" +
+//                "  </JSLIST>";
+//
+//        String b = "Bla7xAv4MZzg9KU+T8ASuFCNnxa2rBtfCuCaRaU1CGqjjV4K/KtOCFUp0z2wgJrKzikhBtNaMvWEYOzWqBuYf8xLESRbm77Q/p6bbx7Scv0vUjX/a3TGbOB63DmCfPqUjE1N+wf74qIoaEF2o5bzQxRX2mCLtHSXI1n3dLeVXD4XxlLFBpfp8vp7hzBWhbiLSp/yQ2j23TF1SQ2yxwoluHp961kLQgMUqwA+1SSWAC8DYxlN599NTe/pEWdz0YYY9tbpIEFGNyArtNIXCmtjiT3i9cZhPqDndj72awGcGUizXfmEg7JGbzq1qgwlMHAyWP2LwXoOSStW1oxs+iOBbW0ZmzBgQC6CiExAv4H6RjPycedFAxNRgEuKSXScuTIwAOpNF3pjoRDmxzgR3WLMR+i3aElZk9CmOMHQn9y0lRA=";
+//        String c = Codec.Encrypt(a);
+//        System.out.println(Codec.Encrypt(a));
+//        System.out.println(Codec.Decrypt(c));
+//        System.out.println(Codec.Encrypt(token));
+//    }
 }
 
 
