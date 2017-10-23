5fd08c53331d4ba78f7737edd4deea4351f6c280
zhengjitf <zhengji1991@gmail.com>
2017-10-23 22:12:57 +0800

    update

---
+++ admin-web/src/components/push/_thumbs/EditDialog.vue
@@ -0,0 +1,22 @@
+<script>
+/*
+ * Created by zhengji
+ * Date: 2017/10/21
+ */
+export default {
+  name: '',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<style lang="scss">
+</style>
