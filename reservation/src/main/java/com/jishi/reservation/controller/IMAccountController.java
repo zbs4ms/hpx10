3d1e8377ae91329579e389ae9dfb49e4445b6b6d
liangxiong <liang.xiong@hpx10.com>
2017-10-29 16:28:00 +0800

    im聊天信息接口

--- reservation/src/main/java/com/jishi/reservation/controller/IMAccountController.java
+++ reservation/src/main/java/com/jishi/reservation/controller/IMAccountController.java
@@ -2,6 +2,8 @@ package com.jishi.reservation.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jishi.reservation.controller.base.MyBaseController;
+import com.jishi.reservation.controller.protocol.IMChatInfo;
+import com.jishi.reservation.dao.models.IMAccount;
 import com.jishi.reservation.service.AccountService;
 import com.jishi.reservation.service.IMAccountService;
 import com.jishi.reservation.service.enumPackage.ReturnCodeEnum;
@@ -43,7 +45,7 @@ public class IMAccountController extends MyBaseController {
             }
         }
 
-        String imToken = imAccountService.getUserToken(accountId);
+        String imToken = imAccountService.getUserIMAccount(accountId).getImToken();
         return ResponseWrapper().addData(imToken).addMessage("ok").ExeSuccess(ReturnCodeEnum.SUCCESS.getCode());
     }
 
@@ -60,7 +62,7 @@ public class IMAccountController extends MyBaseController {
                 return ResponseWrapper().addMessage("登陆信息已过期，请重新登陆").ExeFaild(ReturnCodeEnum.NOT_LOGIN.getCode());
             }
         }
-        String imToken = imAccountService.getDoctorToken(doctorId);
+        String imToken = imAccountService.getDoctorIMAccount(doctorId).getImToken();
         return ResponseWrapper().addData(imToken).addMessage("ok").ExeSuccess(ReturnCodeEnum.SUCCESS.getCode());
     }
 
@@ -96,4 +98,26 @@ public class IMAccountController extends MyBaseController {
         String imToken = imAccountService.refreshDoctorToken(doctorId);
         return ResponseWrapper().addData(imToken).addMessage("ok").ExeSuccess(ReturnCodeEnum.SUCCESS.getCode());
     }
+
+  @ApiOperation(value = "获取聊天信息，医生im账户，用户im账号和token", response = String.class)
+  @RequestMapping(value = "/chatToDocter", method = RequestMethod.PUT)
+  @ResponseBody
+  public JSONObject chatToDocter(HttpServletRequest request, HttpServletResponse response,
+                                       @ApiParam(value = "accountId", required = false) @RequestParam(value = "accountId", required = false) Long accountId,
+                                       @ApiParam(value = "doctorId", required = true) @RequestParam(value = "doctorId", required = true) Long doctorId) throws Exception {
+
+      if (accountId == null) {
+          accountId = accountService.returnIdByToken(request);
+          if(accountId.equals(-1L)){
+              return ResponseWrapper().addMessage("登陆信息已过期，请重新登陆").ExeFaild(ReturnCodeEnum.NOT_LOGIN.getCode());
+          }
+      }
+      IMAccount imUserAccount = imAccountService.getUserIMAccount(accountId);
+      IMAccount imDoctorAccount = imAccountService.getDoctorIMAccount(doctorId);
+      IMChatInfo info = new IMChatInfo();
+      info.setImSourceId(imUserAccount.getImAccId());
+      info.setImDestId(imDoctorAccount.getImAccId());
+      info.setImToken(imUserAccount.getImToken());
+      return ResponseWrapper().addData(info).addMessage("ok").ExeSuccess(ReturnCodeEnum.SUCCESS.getCode());
+  }
 }
