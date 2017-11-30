f69756e30f21b25fc97c1c8f39fe53030d8e250b
c568254965 <568254965@qq.com>
2017-11-30 18:03:47 +0800

    修改过滤

--- reservation/src/main/java/com/jishi/reservation/controller/base/filter/admin/AdminLoginFilter.java
+++ reservation/src/main/java/com/jishi/reservation/controller/base/filter/admin/AdminLoginFilter.java
@@ -8,9 +8,12 @@ import javax.servlet.annotation.WebInitParam;
 /**
  * Created by liangxiong on 2017/11/22.
  */
+//@WebFilter(filterName = "AdminBaseLoginFilter", urlPatterns = {
+
+//        "/ad/*", "/reservation/ad/*"},
 @WebFilter(filterName = "AdminBaseLoginFilter", urlPatterns = {
-      "/ad/*", "/reservation/ad/*"},
+        ""},
       //添加不进行登录验证的url，可以为实际路径或正则表达式，以','分隔
-      initParams = {@WebInitParam(name = VerifyLoginFilter.EXCLUDED_PAGES, value=".*/ad/admin/login")})
+      initParams = {@WebInitParam(name = VerifyLoginFilter.EXCLUDED_PAGES, value=".*/ad/login")})
 public class AdminLoginFilter extends AdminBaseLoginFilter {
 }
