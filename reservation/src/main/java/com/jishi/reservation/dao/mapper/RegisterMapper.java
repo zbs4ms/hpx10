2ee057552931df60a082e810fce7e0afded4bdb8
c568254965 <568254965@qq.com>
2017-12-05 19:04:29 +0800

    修改

--- reservation/src/main/java/com/jishi/reservation/dao/mapper/RegisterMapper.java
+++ reservation/src/main/java/com/jishi/reservation/dao/mapper/RegisterMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -37,4 +38,9 @@ public interface RegisterMapper extends MyMapper<Register> {
             "select * from register where id = #{registerId}"
     })
     Register queryById(@Param("registerId") Long registerId);
+
+    @Select({
+            "select * from register where br_id = #{brid} and agreed_time = #{agreeDate} and doctor_id = #{doctorId}"
+    })
+    Register queryByBrIdTimeDoctorId(@Param("brid") String brid,@Param("agreeDate") Date agreeDate,@Param("doctorId") String doctorId);
 }
